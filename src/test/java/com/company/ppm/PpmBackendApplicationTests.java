1252c7253fe0e8353ad3fec99d67ffb262cf9d08
PC <mike.saviour08@gmail.com>
2026-02-24 06:13:23 +0100

    Initial commit

---
+++ src/test/java/com/company/ppm/PpmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.ppm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
